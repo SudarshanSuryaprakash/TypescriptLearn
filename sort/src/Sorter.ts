52a992934dc86092a6f4fc9307beb28ea3e5e426
Sudarshan Suryaprakash <sudarshan16rms@gmail.com>
2021-07-30 19:04:10 +0100

    Refactor(sort/Sorter):Made Sorter an abstract class so that sort can be called easily on instances of classes extending Sorter

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,18 +1,19 @@
-
 export interface Sortable {
-  length: number
-  compare(left: number, right: number): boolean
-  swap(left: number, right: number): void
+  length: number;
+  compare(left: number, right: number): boolean;
+  swap(left: number, right: number): void;
 }
 
-export class Sorter {
-  constructor(public collection: Sortable){}
+export abstract class Sorter {
+  abstract compare(left: number, right: number): boolean;
+  abstract length: number;
+  abstract swap(left: number, right: number): void;
 
   sort() {
-    for(let i = 0; i < this.collection.length; i++) {
-      for(let j = 0; j < this.collection.length - i - 1; j++){
-        if(this.collection.compare(j,j+1)) {
-          this.collection.swap(j,j+1)
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,23 +1,19 @@
 import { NumbersCollection } from "./NumbersCollection";
-import { Sorter } from "./Sorter";
-import {CharactersCollection} from './CharacterCollection';
-import {LinkedList} from "./LinkedList";
+import { CharactersCollection } from "./CharacterCollection";
+import { LinkedList } from "./LinkedList";
 
-const numbers = new NumbersCollection([1,-3,5,4])
-const sorter = new Sorter(numbers)
-sorter.sort();
-console.log(sorter.collection)
+const numbers = new NumbersCollection([1, -3, 5, 4]);
+numbers.sort();
+console.log(numbers.data);
 
-const stringInput = new CharactersCollection('Sudarshan')
-const charSorter = new Sorter(stringInput)
-charSorter.sort();
-console.log(charSorter.collection)
+const stringInput = new CharactersCollection("Sudarshan");
+stringInput.sort();
+console.log(stringInput.data);
 
-const linkedList = new LinkedList()
-linkedList.add(200)
-linkedList.add(-20)
-linkedList.add(20)
-linkedList.add(50)
-const linkedSorter = new Sorter(linkedList)
-linkedSorter.sort()
-console.log(linkedSorter.collection)
+const linkedList = new LinkedList();
+linkedList.add(200);
+linkedList.add(-20);
+linkedList.add(20);
+linkedList.add(50);
+linkedList.sort();
+linkedList.print();
