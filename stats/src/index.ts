ba50b262c67ff751bdb6a28ae7d9b47bdff94217
Sudarshan Suryaprakash <sudarshan16rms@gmail.com>
2021-07-31 14:50:22 +0100

    chore(stats/{CsvFileReader,index}):Extracted CSV file reading logic and included an enum for better readability

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,16 +1,19 @@
-import fs from 'fs'
+import {CsvFileReader} from './CsvFileReader'
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-}).split('\n').map((row: string): string[] => {
-  return row.split(',')
-})
+const reader = new CsvFileReader('football.csv')
+reader.read()
 
 let manWins = 0;
 
-for(let match of matches) {
-  if(match[1] === 'Man United' && match[5] === 'H') manWins++;
-  else if(match[2] === 'Man United' && match[5] === 'A') manWins++;
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+for(let match of reader.data) {
+  if(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) manWins++;
+  else if(match[2] === 'Man United' && match[5] === MatchResult.AwayWin) manWins++;
 }
 
 console.log(manWins)
